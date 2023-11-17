67b80d6eb3eabadfff37d186017936a43fd24ae2
Duy Thai <sb23092002@gmail.com>
2023-11-17 12:02:52 +0700

    updat

--- WeekOne/InternPartOne/src/Maze.java
+++ WeekOne/InternPartOne/src/Maze.java
@@ -1,12 +1,10 @@
+import java.awt.*;
 import java.util.LinkedList;
 import java.util.Queue;
-
-import java.awt.*;
 import javax.swing.*;
 
-public class ExerciseFive {
+public class Maze {
     public static void main(String[] args) {
-        //
         JFrame frame = new JFrame();
         frame.setSize(650, 470);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,7 +15,7 @@ public class ExerciseFive {
 
     public static class MazePanel extends JPanel {
         private static final long serialVersionUID = -566807999447681130L;
-        private int[][] maze = { // khởi tạo ma trận mảng 2 chiều
+        private int[][] maze = {
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
                 { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
@@ -42,37 +40,21 @@ public class ExerciseFive {
         private int sizeh, sizew, start, end;
 
         public MazePanel() {
-            // Kích thước ma trận
             sizeh = 21;
             sizew = 31;
             start = 10;
             end = 0;
             solve();
-            repaint(); // vẽ ma trận và lời giải
+            repaint();
         }
 
-        /**
-         * Implement một phương pháp tìm đường nào đó.
-         * <p>
-         * Yêu cầu : Vẽ đường đi từ điểm bắt đầu đến điểm kết thúc. Không hiện
-         * các phần thừa - là các phần đường cụt hoặc đường đi bị sai. Chỉ vẽ
-         * tuyến đường chính đi từ điểm đầu (màu vàng) đến màu đỏ.
-         * <p>
-         * Đường đi từ điểm đầu đến điểm cuối được tô màu xanh dương, để tô màu
-         * xanh dương hãy set giá trị của điểm trên ma trận sang một số > 2
-         */
         public void solve() {
-            // Hàm này chứa phương pháp tìm đường từ điểm start đến vị
-            // trí màu đỏ trên ma trận
-            dfs(start, end);
-//            boolean foundPath = dfs(start, end);
+            bfs(start, end);
         }
 
-
-        public void paintComponent(Graphics g) // vẽ ma trận + lời giải
-        {
+        public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            for (int j = 0; j < sizew; j++){
+            for (int j = 0; j < sizew; j++) {
                 for (int i = 0; i < sizeh; i++) {
                     if (i == start && j == end)
                         g.setColor(Color.yellow);
@@ -82,48 +64,60 @@ public class ExerciseFive {
                         g.setColor(Color.black);
                     else if (maze[i][j] == 2)
                         g.setColor(Color.red);
-                    else
-                        g.setColor(Color.blue); // blue là màu của lời giải
+                    else if (maze[i][j] == 3)
+                        g.setColor(Color.blue); // blue is the color of the solution path
                     g.fillRect(j * 20, i * 20, 20, 20);
                 }
             }
         }
-        private boolean dfs(int x, int y) {
-            // Kiểm tra xem đã đến điểm cuối chưa
-            if (maze[x][y] == 2) {
-//                maze[x][y] = 2; // Đánh dấu đường đi
-                return true;
+
+        private void bfs(int startX, int startY) {
+            Queue<Point> queue = new LinkedList<>();
+            queue.add(new Point(startX, startY));
+
+            int[][] distance = new int[sizeh][sizew];
+            for (int i = 0; i < sizeh; i++) {
+                for (int j = 0; j < sizew; j++) {
+                    distance[i][j] = Integer.MAX_VALUE;
+                }
             }
-            // Kiểm tra xem vị trí hiện tại có thể di chuyển không
-            if (isValidMove(x, y)) {
-                maze[x][y] = 3; // Đánh dấu đường đi
-                //Dưới
-                if (dfs(x + 1, y))
-                    return true;
-                //Lên trên
-                if (dfs(x - 1, y))
-                    return true;
-                //Phải
-                if (dfs(x, y + 1))
-                    return true;
-                //Trái
-                if (dfs(x, y - 1))
-                    return true;
-                // Nếu không có đường đi nào từ vị trí hiện tại, quay lui
-                maze[x][y] = 0;  // Bỏ đánh dấu
+            distance[startX][startY] = 0;
+
+            int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+            while (!queue.isEmpty()) {
+                Point current = queue.poll();
+
+                for (int[] dir : directions) {
+                    int newX = current.x + dir[0];
+                    int newY = current.y + dir[1];
+
+                    if (isValidMove(newX, newY) && distance[newX][newY] == Integer.MAX_VALUE) {
+                        distance[newX][newY] = distance[current.x][current.y] + 1;
+                        queue.add(new Point(newX, newY));
+                    }
+                }
             }
 
-            return false;
+            // Backtrack to find the shortest path
+            int x = end;
+            int y = start;
+            while (distance[x][y] != 0) {
+                maze[x][y] = 3; // Mark the solution path
+                for (int[] dir : directions) {
+                    int newX = x + dir[0];
+                    int newY = y + dir[1];
+                    if (isValidMove(newX, newY) && distance[newX][newY] == distance[x][y] - 1) {
+                        x = newX;
+                        y = newY;
+                        break;
+                    }
+                }
+            }
         }
-        private boolean isValidMove(int x, int y) {
-            // Kiểm tra xem đã đến điểm cuối chưa
-            if (maze[x][y] == 2) {
-//                maze[x][y] = 2; // Đánh dấu đường đi
-                return true;
-            }
-            // Kiểm tra xem vị trí có hợp lệ không
-            return x >= 0 && x < sizeh && y >= 0 && y < sizew && maze[x][y] == 0;
 
+        private boolean isValidMove(int x, int y) {
+            return x >= 0 && x < sizeh && y >= 0 && y < sizew && maze[x][y] == 0;
         }
     }
 }
