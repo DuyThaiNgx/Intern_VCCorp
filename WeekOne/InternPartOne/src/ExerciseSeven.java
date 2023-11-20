6f95bbc5858fd93ef248bfb8c451b3f6754e6f0e
Duy Thai <sb23092002@gmail.com>
2023-11-20 18:20:50 +0700

    finish

--- WeekOne/InternPartOne/src/ExerciseSeven.java
+++ WeekOne/InternPartOne/src/ExerciseSeven.java
@@ -7,26 +7,26 @@ public class ExerciseSeven {
         String regex = "^https?://(www.)?[a-zA-Z0-9-]+(\\.[a-z]{2,6})(/[^\\s]*)?$";
         String regexCheck = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)";
 
-        Pattern pattern = Pattern.compile(regex1);
-        String url = "http://tiki.vn/dien-thoai-may-tinh-bang/c1789?src=mega-menu";
-        System.out.println(url.matches(regexCheck));
+        Pattern pattern = Pattern.compile(regexCheck);
+        String url = "http://m.tiki.vn/dien-thoai-may-tinh-bang/c1789?src=mega??-menu#2";
+//        System.out.println(url.matches(regexCheck));
         Matcher matcher = pattern.matcher(url);
 //        System.out.println(matcher.matches());
-//        if(matcher.matches()){
-//            System.out.println("URL is valid");
-//        }
-//        else{
-//            System.out.println("URL is unvalid");
-//        }
+        if(matcher.matches()){
+            System.out.println("URL is valid");
+        }
+        else{
+            System.out.println("URL is invalid");
+        }
 
-        String text2 = "2/12/2018";
-        String text3 = "12/12/aaaa";
-        pattern = Pattern.compile("^\\d{1,2}[-|/]\\d{1,2}[-|/]\\d{4}$");
-        System.out.println("\nChuỗi " + text2 + " có định dạng ngày tháng: "
-                + pattern.matcher(text2).matches());
-
-        System.out.println("Chuỗi " + text3 + " có định dạng ngày tháng: "
-                + pattern.matcher(text3).matches());
+//        String text2 = "2/12/2018";
+//        String text3 = "12/12/aaaa";
+//        pattern = Pattern.compile("^\\d{1,2}[-|/]\\d{1,2}[-|/]\\d{4}$");
+//        System.out.println("\nChuỗi " + text2 + " có định dạng ngày tháng: "
+//                + pattern.matcher(text2).matches());
+//
+//        System.out.println("Chuỗi " + text3 + " có định dạng ngày tháng: "
+//                + pattern.matcher(text3).matches());
         //(pattern.matcher(text3).matches()
         // bằng với Matcher matcher = pattern.matcher(url) sau đó matcher.matches() tại sao lại khác nhau
         //
