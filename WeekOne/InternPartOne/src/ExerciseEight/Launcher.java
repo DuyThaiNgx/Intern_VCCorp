f6cd1f73075dfe9a6faf01b14ebc18f9ac592be8
Duy Thai <sb23092002@gmail.com>
2023-11-20 11:30:47 +0700

    add more ex

--- WeekOne/InternPartOne/src/ExerciseEight/Launcher.java
+++ WeekOne/InternPartOne/src/ExerciseEight/Launcher.java
@@ -3,9 +3,11 @@ package ExerciseEight;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Launcher {
-    public static void addToFile(){
+    public static void addToFile(String filePath){
         List<Country> countries = new ArrayList<>();
         Country country1 = new Country("VN", "Vietnam", "Asia", 331212.0, 97338579, 261921.9, 1);
         Country country2 = new Country("US", "United States", "North America", 9833517.0, 331002651, 21433225.0, 2);
@@ -20,11 +22,12 @@ public class Launcher {
         countries.add(country1);
         countries.add(country2);
         countries.add(country3);
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src/ExerciseEight/countries.dat"))) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
             for (Country country : countries){
                 oos.writeObject(country);
             }
             System.out.println("Saved to countries.dat" );
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -42,16 +45,21 @@ public class Launcher {
         }
     }
 
+
     public static void main(String[] args) {
-        addToFile();
+        String countriesFile = "src/ExerciseEight/countries.dat";
+        String citiesFile = "src/ExerciseEight/cities.dat";
+        addToFile(countriesFile);
+        addToFile(citiesFile);
+
         List<City> listCity = new ArrayList<>();
         List<Country> listCountry = new ArrayList<>();
 
         try {
-            FileInputStream countryFile = new FileInputStream("src/ExerciseEight/countries.dat");
+            FileInputStream countryFile = new FileInputStream(citiesFile);
             ObjectInputStream countryInput = new ObjectInputStream(countryFile);
 
-            FileInputStream cityFile = new FileInputStream("src/ExerciseEight/cities.dat");
+            FileInputStream cityFile = new FileInputStream(citiesFile);
             ObjectInputStream cityInput = new ObjectInputStream(cityFile);
 
             // Đọc danh sách quốc gia từ file
@@ -82,5 +90,91 @@ public class Launcher {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        findMostPopulousCityPerCountry(listCountry, listCity);
+        findMostPopulousCityPerContinent(listCountry, listCity);
+        findMostPopulousCapitalCity(listCountry, listCity);
+        findMostPopulousCapitalCityPerContinent(listCountry, listCity);
+    }
+    private static void findMostPopulousCityPerCountry(List<Country>listCountry, List<City> listCity) {
+        Map<String, City> mostPopulousCitiesPerCountry = listCity.stream()
+                .collect(Collectors.toMap(city -> city.getName(),
+                        city -> city,
+                        (existing, replacement) -> existing.getPopulation() > replacement.getPopulation() ? existing : replacement));
+
+        listCountry.forEach(country -> {
+            City mostPopulousCity = mostPopulousCitiesPerCountry.get(country.getName());
+            System.out.println("Most populous city in " + country.getName() + ": " + mostPopulousCity.getName());
+        });
+    }
+
+    private static void findMostPopulousCityPerContinent(List<Country> countries, List<City> cities) {
+        Map<String, City> mostPopulousCitiesPerContinent = countries.stream()
+                .collect(Collectors.toMap(country -> country.getContinent(),
+                        country -> cities.stream()
+                                .filter(city -> city.getName().equals(country.getName()))
+                                .max((c1, c2) -> Integer.compare(c1.getPopulation(), c2.getPopulation()))
+                                .orElse(null),
+                        (existing, replacement) -> existing.getPopulation() > replacement.getPopulation() ? existing : replacement));
+
+        mostPopulousCitiesPerContinent.forEach((continent, city) -> {
+            System.out.println("Most populous city in continent " + continent + ": " + city.getName());
+        });
+    }
+
+    private static void findMostPopulousCapitalCity(List<Country> countries, List<City> cities) {
+        Map<String, City> mostPopulousCapitalCities = countries.stream()
+                .collect(Collectors.toMap(country -> country.getName(),
+                        country -> cities.stream()
+                                .filter(city -> city.getId() == country.getCapital())
+                                .findFirst()
+                                .orElse(null),
+                        (existing, replacement) -> existing.getPopulation() > replacement.getPopulation() ? existing : replacement));
+
+        mostPopulousCapitalCities.forEach((country, city) -> {
+            System.out.println("Most populous capital city in " + country + ": " + city.getName());
+        });
+    }
+
+    private static void findMostPopulousCapitalCityPerContinent(List<Country> countries, List<City> cities) {
+        Map<String, City> mostPopulousCapitalCitiesPerContinent = countries.stream()
+                .collect(Collectors.toMap(country -> country.getContinent(),
+                        country -> cities.stream()
+                                .filter(city -> city.getId() == country.getCapital())
+                                .findFirst()
+                                .orElse(null),
+                        (existing, replacement) -> existing.getPopulation() > replacement.getPopulation() ? existing : replacement));
+
+        mostPopulousCapitalCitiesPerContinent.forEach((continent, city) -> {
+            System.out.println("Most populous capital city in continent " + continent + ": " + city.getName());
+        });
+    }
+
+//    private static void sortCountriesByNumberOfCities(List<Country> countries) {
+//        List<Country> sortedCountries = countries.stream()
+//                .sorted((c1, c2) -> Integer.compare(getCityCount(c2), getCityCount(c1)))
+//                .collect(Collectors.toList());
+//
+//        System.out.println("Countries sorted by number of cities:");
+//        sortedCountries.forEach(country -> System.out.println(country.getName() + ": " + getCityCount(country)));
+//    }
+//
+//    private static int getCityCount(Country country, List<City> cities) {
+//        // Assume cities is a list of City objects
+//        return (int) cities.stream().filter(city -> city.getName().equals(country.getName())).count();
+//    }
+
+    private static void sortCountriesByPopulationDensity(List<Country> countries) {
+        List<Country> sortedCountries = countries.stream()
+                .filter(country -> country.getPopulation() > 0)
+                .sorted((c1, c2) -> Double.compare(getPopulationDensity(c2), getPopulationDensity(c1)))
+                .collect(Collectors.toList());
+
+        System.out.println("Countries sorted by population density:");
+        sortedCountries.forEach(country -> System.out.println(country.getName() + ": " + getPopulationDensity(country)));
+    }
+
+    private static double getPopulationDensity(Country country) {
+        return country.getPopulation() / country.getSurfaceArea();
     }
 }
---
+++ WeekOne/InternPartOne/src/ExerciseThree.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
+class WordCountTask implements Callable<Map<String, Integer>> {
+    private final File file;
+
+    public WordCountTask(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public Map<String, Integer> call() throws Exception {
+        Map<String, Integer> wordCount = new HashMap<>();
+
+//        try {
+//            List<String> lines = Files.readAllLines(Paths.get(file.getAbsolutePath()));
+//            for (String line : lines) {
+//                String[] words = line.split("\\s+");
+//                for (String word : words) {
+//                    wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return wordCount;
+    }
+}
+
+public class ExerciseThree {
+    public static void main(String[] args) {
+        String folderPath = "src/BaiTapPhan1/input_3";
+        List<File> files = getFilesInFolder(folderPath);
+        //Set toi da 6 luong chay
+        ExecutorService executorService = Executors.newFixedThreadPool(6);
+        List<Future<Map<String, Integer>>> futures = new ArrayList<>();
+
+        for (File file : files) {
+            WordCountTask task = new WordCountTask(file);
+            Future<Map<String, Integer>> future = executorService.submit(task);
+            futures.add(future);
+        }
+
+        Map<String, Integer> totalWordCount = new HashMap<>();
+
+        for (Future<Map<String, Integer>> future : futures) {
+            try {
+                Map<String, Integer> wordCount = future.get();
+                mergeWordCount(totalWordCount, wordCount);
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        executorService.shutdown();
+
+        // Sort and print the top 10 most and least frequent words
+        printTopWords(totalWordCount, 10, true);
+        printTopWords(totalWordCount, 10, false);
+    }
+
+    private static List<File> getFilesInFolder(String folderPath) {
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles(pathname -> pathname.getName().endsWith(".txt"));
+        return files != null ? Arrays.asList(files) : Collections.emptyList();
+    }
+
+    private static void mergeWordCount(Map<String, Integer> totalWordCount, Map<String, Integer> wordCount) {
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            totalWordCount.put(entry.getKey(), totalWordCount.getOrDefault(entry.getKey(), 0) + entry.getValue());
+        }
+    }
+
+    private static void printTopWords(Map<String, Integer> wordCount, int topN, boolean mostFrequent) {
+        Comparator<Map.Entry<String, Integer>> comparator =
+                mostFrequent ? Map.Entry.comparingByValue(Comparator.reverseOrder()) :
+                        Map.Entry.comparingByValue();
+
+        List<Map.Entry<String, Integer>> sortedEntries = wordCount.entrySet()
+                .stream()
+                .sorted(comparator)
+                .limit(topN)
+                .collect(Collectors.toList());
+
+        if (mostFrequent) {
+            System.out.println("Top " + topN + " Most Frequent Words:");
+        } else {
+            System.out.println("Top " + topN + " Least Frequent Words:");
+        }
+
+        for (Map.Entry<String, Integer> entry : sortedEntries) {
+            System.out.println("Word: "+ "'" +entry.getKey() + "'" + ": " + entry.getValue() + " times");
+        }
+
+    }
+}
